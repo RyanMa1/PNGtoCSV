29cfdeca406265cfd9b305396ceced1a0503caad
Ryan <ryama@ucdavis.edu>
2020-01-19 02:23:32 -0800

    script png to csv

--- PNGtoCSV.py
+++ PNGtoCSV.py
@@ -1,19 +1,60 @@
+# import numpy as np
+# import cv2
+# import os
+
+# IMG_DIR = './doritos/._1.71FsTo%2BzenL._SL1000_.jpg'
+# for img in os.listdir(IMG_DIR):
+#         img_array = cv2.imread(os.path.join(IMG_DIR,img), cv2.IMREAD_GRAYSCALE)
+
+#         img_array = (img_array.flatten())
+
+#         img_array  = img_array.reshape(-1, 1).T
+
+#         print(img_array)
+
+#         with open('output.csv', 'ab') as f:
+
+#             np.savetxt(f, img_array, delimiter=",")
+
+from PIL import Image
 import numpy as np
-import cv2
+import sys
 import os
+import csv
 
-IMG_DIR = '/home/kushal/Documents/opencv_tutorials/image_reading/dataset'
+#Useful function
+def createFileList(myDir, format='.jpg'):
+    fileList = []
+    print(myDir)
+    for root, dirs, files in os.walk(myDir, topdown=False):
+        for name in files:
+            if name.endswith(format):
+                fullName = os.path.join(root, name)
+                fileList.append(fullName)
+    return fileList
 
-for img in os.listdir(IMG_DIR):
-        img_array = cv2.imread(os.path.join(IMG_DIR,img), cv2.IMREAD_GRAYSCALE)
+# load the original image
+myFileList = createFileList('./doritos')
 
-        img_array = (img_array.flatten())
+for file in myFileList:
+    print(file)
+    img_file = Image.open(file)
+    # img_file.show()
 
-        img_array  = img_array.reshape(-1, 1).T
+    # get original image parameters...
+    width, height = img_file.size
+    format = img_file.format
+    mode = img_file.mode
 
-        print(img_array)
+    # Make image Greyscale
+    img_grey = img_file.convert('L')
+    #img_grey.save('result.png')
+    #img_grey.show()
 
-        with open('output.csv', 'ab') as f:
-
-            np.savetxt(f, img_array, delimiter=",")
-
+    # Save Greyscale values
+    value = np.asarray(img_grey.getdata(), dtype=np.int).reshape((img_grey.size[1], img_grey.size[0]))
+    value = value.flatten()
+    print(value)
+    with open("img_pixels.csv", 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(value)
